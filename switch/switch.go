4c37b16f60bb7e43e25f8cf06396caa55d15eb89
thandon263 <ncubethando20@gmail.com>
2019-03-05 15:56:32 -0500

    Working on the switch for date and interfaces

--- switch/switch.go
+++ switch/switch.go
@@ -23,4 +23,27 @@ func main() {
 	default:
 		fmt.Println("It's a weekday")
 	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	case t.Hour() > 12:
+		fmt.Println("It's After noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
